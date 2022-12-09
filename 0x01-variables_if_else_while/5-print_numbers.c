181167af313c3b17f561468de42b1234f276ac15
Precious Aitanun <paitanun35@gmail.com>
2022-12-09 09:05:47 -0800

    5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
