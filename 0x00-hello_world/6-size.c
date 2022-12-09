5fcbe979c11132d3de3e23112bbb53e8fb826984
Precious Aitanun <paitanun35@gmail.com>
2022-12-08 21:14:40 -0800

    6-size.c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
