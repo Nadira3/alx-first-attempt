41b95d33f40f1faaaca1cfe08a2f03273f039caa
Precious Aitanun <paitanun35@gmail.com>
2022-12-09 11:56:07 -0800

    9-print_comb commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point of rhe program
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
