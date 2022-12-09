a1b449dcbc545845020fd9711467355914782e0b
Precious Aitanun <paitanun35@gmail.com>
2022-12-09 09:31:51 -0800

    6-print_numberz commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		int putchar(int c);
+	}
+	putchar('\n');
+
+	return (0);
+}
