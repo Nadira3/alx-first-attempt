327adce52c249b84527f32a9ee84b128c4d35106
Precious Aitanun <paitanun35@gmail.com>
2022-12-09 08:24:34 -0800

    3 print commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,17 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
+
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
+
 	return (0);
 }
