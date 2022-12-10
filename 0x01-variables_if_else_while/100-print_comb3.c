e550276193eb059b14c9717874116f8e5ce115b7
Precious Aitanun <paitanun35@gmail.com>
2022-12-09 19:50:42 -0800

    100-print_comb3 commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - entry point of the program
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+		
+		if (a == 8 && b == 9)
+		{
+			continue;
+		}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
