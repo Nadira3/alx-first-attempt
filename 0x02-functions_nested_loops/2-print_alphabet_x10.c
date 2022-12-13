af2abdf7dde2a415a29329f128fb95eb40356be6
Precious Aitanun <paitanun35@gmail.com>
2022-12-13 12:33:51 -0800

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-    char ch, i;
-    for (i = 0; i < 10; i++)
-    {
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-	_putchar('\n');
-    }
+	char ch, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
 }
