5a9850ea6a02ad16457ba2e32e50498e78767686
Precious Aitanun <paitanun35@gmail.com>
2022-12-13 04:17:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include "_putchar.h"
+#include "0-putchar.c"
+/** 
+ * _putchar - writes the character c to stdout 
+ * @c: The character to print
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+int main(int b, p, u, t, c, h, a, r);
