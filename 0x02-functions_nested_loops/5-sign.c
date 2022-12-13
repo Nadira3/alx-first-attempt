81348ff0acfd06732861466a1da09477534f4023
Precious Aitanun <paitanun35@gmail.com>
2022-12-13 15:01:35 -0800

    5-sign commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,17 @@
 /**
  * print_sign - prints the sign of a number
  *
- * @c: stores the value of the input
+ * @n: stores the value of the input
  * Return: 0 || 1
  */
 int print_sign(int n)
 {
-	if(n == 0)
+	if (n == 0)
 	{
 		_putchar(n + '0');
 		return (0);
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);
