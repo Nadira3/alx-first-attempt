484e06577a7f36c35147405ef172b69f2ac9cef4
Precious Aitanun <paitanun35@gmail.com>
2022-12-13 19:44:57 -0800

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,25 +3,24 @@
 /**
  * times_table - prints d nine timez table
  *
- * @n: stores value
  * Return: void
  */
 void times_table(void)
 {
 	int i;
 	int j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int k = i * j;
-		if (i == 9 && j == 9)
-			continue;
-                printf("%d", k);	
-		_putchar(',');
-		_putchar(' ');
+
+			printf("%d", k);
+			if (j != 9)
+			printf(", ");
 		}
-		_putchar('\n');
+		printf("\n");
 
 
 	}
