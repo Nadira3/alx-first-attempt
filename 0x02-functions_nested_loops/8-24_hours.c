11cfda2b63f850e8073f8ce35b88cf738c6431fe
Precious Aitanun <paitanun35@gmail.com>
2022-12-14 19:41:37 -0800

    8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,23 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (j = 0; j < 60; j++)
+		j = 0;
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
+
+			j++;
 		}
+		i++;
 	}
+
 }
