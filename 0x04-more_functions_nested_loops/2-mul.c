17ad0064cf9238df7ddef3ad8ce2e51b75fd8da5
Precious Aitanun <paitanun35@gmail.com>
2022-12-15 11:11:55 -0800

    2-mul commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: stores values
+ * @b: stores values
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,4 @@
 int _isupper(int c);
 int _putchar(char);
 int _isdigit(int c);
+int mul(int a, int b);
