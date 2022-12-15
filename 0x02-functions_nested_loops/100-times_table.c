4bb6336775aa096439d751a7e69717f1b611ba9f
Precious Aitanun <paitanun35@gmail.com>
2022-12-14 19:27:29 -0800

    12 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,7 @@ void print_times_table(int n)
 	int j;
 
 	if (n == 0)
-	{
 		printf("%d\n", n);
-	}
-	
 	if (n > 0 && n <= 15)
 	{
 	for (i = 0; i <= n; i++)
@@ -32,7 +29,6 @@ void print_times_table(int n)
 				else
 					printf(" %d,", k);
 			}
-			
 			else if (j == 0)
 				printf("%d,", k);
 			else
