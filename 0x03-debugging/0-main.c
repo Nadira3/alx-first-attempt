787725340cab72b21697d20debb47fdb69ac126e
Precious Aitanun <paitanun35@gmail.com>
2022-12-14 22:41:08 -0800

    debug commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,12 @@
 * Return: 0
 */
 
-int main()
+int main(void)
 {
         int i;
 
         i = 98;
-        positive_or_negative(i);
+        int positive_or_negative(i);
 
         return (0);
 }
