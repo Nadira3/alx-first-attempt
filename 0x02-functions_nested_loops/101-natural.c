1e2198eb9e8b066179da6555154f568927a80d88
Precious Aitanun <paitanun35@gmail.com>
2022-12-14 20:19:16 -0800

    13 commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,21 +2,21 @@
 /**
  * main - entry point of the program
  *
- * Return: sum
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 	int sum = 0;
 
-	while(i < 1024)
-    {
-      if ((i % 3 == 0) || ( i % 5 == 0))
-      {
-         sum = sum+ i;
-      }
-      i++;
-    }
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+		i++;
+	}
 	printf("%d\n", sum);
 	return (0);
 }
