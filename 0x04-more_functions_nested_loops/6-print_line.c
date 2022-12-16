a9df5f3a3f6cf43121fddb07162c9ae01bba64a6
Precious Aitanun <paitanun35@gmail.com>
2022-12-15 19:04:50 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - prints n number of lines
+ *
+ * @n: stores value of input
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			break;
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
