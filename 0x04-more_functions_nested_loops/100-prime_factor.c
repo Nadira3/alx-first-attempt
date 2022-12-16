5be6c289ed722392a4d5588691a6b39e8c47bb3c
Precious Aitanun <paitanun35@gmail.com>
2022-12-16 14:13:33 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * prime - prints the prime factors of n
+ *
+ * @n: stores the integer value
+ */
+void prime(long int n)
+{
+	int i;
+
+	for (i = 2; n > 1; i++)
+	{
+		int k;
+
+		while (n % i == 0)
+		{
+			k = i;
+			n = n / k;
+
+			if (n == 1)
+				printf("%d\n", k);
+		}
+	}
+}
+/**
+ * main - entry of the program
+ */
+int main(void)
+{
+	prime(612852475143);
+	return (0);
+}
