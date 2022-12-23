84afc2390f5237293ff375b5cf362b69b3c1bad5
Precious Aitanun <paitanun35@gmail.com>
2022-12-23 08:28:49 -0800

    7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * cap_string: capitalizes the firat letter of each word in a string
+ * cap_string- capitalizes the first letter of each word in a string
  *
  * @s: string
  * Return: character
@@ -9,13 +9,19 @@ char *cap_string(char *s)
 {
 	int i;
 
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] -= 32;
+
 	for (i = 0; s[i]; i++)
 	{
-		if ((s[i - 1] = ' '))
-		{
-			if (s[i] >= 97 && s[i] <= 122)
-				s[i] -= 32;
-		}
+		if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == ' ')
+			s[i] -= 32;
+		else if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == '.')
+			s[i] -= 32;
+		else if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == '\n')
+			s[i] -= 32;
+		else if (s[i] >= 97 && s[i] <= 122 && s[i - 1] == '\t')
+			s[i] -= 32;
 	}
 	return (s);
 }
