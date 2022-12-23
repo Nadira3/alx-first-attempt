ac4049abfe3066b0e6fec853d123f958c1e36871
Precious Aitanun <paitanun35@gmail.com>
2022-12-22 18:31:30 -0800

    3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-
-	while (*src && i < n)
-	{
-		*dest = *src;
-		src++;
-		dest++;
+	int i;
+	for (i = 0; i < n; i++)
+	{	
+		while (*src)
+		{
+			*dest = *src;
+			src++;
+			dest++;
+		}
 	}
 	*dest = '\0';
 	return (dest);
