b010f41d77764f17dfdab7e881419df791262519
Precious Aitanun <paitanun35@gmail.com>
2022-12-31 02:31:45 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,52 +1,29 @@
 #include <stdio.h>
 /**
- * fibonacci - prints fibonacci numbers
- *
- * @n: stores integer value
- */
-void print_number(int n)
-{
-	unsigned int m = n;
-
-	if (n < 0)
-	{
-		m = -m;
-	}
-
-	if (m / 10)
-		print_number(m / 10);
-	putchar(m % 10 + '0');
-}
-
-void fibonacci(int n)
-{
-	int a = 0;
-	int b = 1;
-	int i;
-
-	for (i = 1; i <= n; i++)
-	{
-		unsigned long int c = a + b;
-		
-		if (i != n)
-		{
-			print_number(c);
-			printf(", ");
-		}
-		else
-			print_number(c);
-
-		a = b;
-		b = c;
-	}
-}
-/**
- * main - entry point of the program
+ * main - entry point of program. prints 50 fibonacci numbers
  *
  * Return: 0
  */
 int main(void)
 {
-	fibonacci(50);
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int sum;
+	int count = 0;
+
+	printf("%lu, %lu, ", i, j);
+	while (count < 48)
+	{
+		sum = i + j;
+		if (count != 47)
+		{
+			printf("%lu, ", sum);
+		}
+		else
+			printf("%lu", sum);
+		i = j;
+		j = sum;
+		count++;
+	}
 	return (0);
 }
