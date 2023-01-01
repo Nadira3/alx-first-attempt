2bf7887da23f06330b82b05e99dea1a2287490db
Precious Aitanun <paitanun35@gmail.com>
2023-01-01 01:17:40 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point of program. prints 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i = 1;
+	unsigned long int j = 2;
+	unsigned long int sum;
+	int count = 0;
+
+	printf("%lu, %lu, ", i, j);
+	while (count < 97)
+	{
+		sum = i + j;
+		if (count != 96)
+		{
+			printf("%lu, ", sum);
+		}
+		else
+			printf("%lu\n", sum);
+		i = j;
+		j = sum;
+		count++;
+	}
+	return (0);
+}
