371adca6faff7b6da811dc3e67ba34f7a554e253
Precious Aitanun <paitanun35@gmail.com>
2023-01-03 14:40:00 -0800

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,36 +1,49 @@
 #include "main.h"
-
+/**
+ * _strlen - finds the length of string
+ *
+ * @s: string
+ * Return: string length
+ */
 int _strlen(char *s)
 {
-     int i = 0;
-     while (s[i] != '\0')
-     {
-         s++;
-         i++;
-     }
-     return (i);
- }
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strspn - finds the length of a substring
+ *
+ * @s: substring
+ * @accept: string
+ * Return: integer
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 	unsigned int n = 0;
 	unsigned int *p = &n;
-    
-    for (i = 0; i < _strlen(s); i++)
-    {
-	    if (s[i] != ' ')
-	    {
-        	for (j = 0; j < _strlen(accept); j++)
-        	{
-	    	if (s[i] == accept[j])
-            	{
-		    *p += 1;
-            	}
-	    	else
-		    continue;
-        	}
-	    }
-    }
-    return (n);
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (s[i] != ' ')
+		{
+			for (j = 0; j < _strlen(accept); j++)
+			{
+				if (s[i] != accept[j])
+				{
+					break;
+				}
+				else
+					*p += 1
+			}
+		}
+	}
+	return (n);
 }
 
