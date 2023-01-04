388926db1803fef6ad6f7f689968adb61f740ba9
Precious Aitanun <paitanun35@gmail.com>
2023-01-03 17:38:09 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 /**
  * this file contains a list of all my function prototypes
  */
-
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
