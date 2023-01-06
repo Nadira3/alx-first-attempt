890e537580b7a1ca4442562a1b167d43210cc9c3
Precious Aitanun <paitanun35@gmail.com>
2023-01-05 20:42:10 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,29 +5,16 @@
  * @n: number
  * Return: integer
  */
-void print_number(int n)
-{
-	unsigned int m = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		m = -m;
-	}
-	if (m / 10)
-		print_number(m / 10);
-	_putchar(m % 10 + '0');
-}
 int factorial(int n)
 {
 	int result;
 
-	if (n <= 0)
+	if (n == 0)
 		result = 1;
+	else if (n < 0)
+		result = -1;
 	else
 		result = n * factorial(n -1);
-	print_number(result);
-	
-	return (0);
+	return (result);
 	
 }
