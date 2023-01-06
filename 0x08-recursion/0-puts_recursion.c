0dc0a5d4a7561cef99160851aeb7f2b1d948b1b3
Precious Aitanun <paitanun35@gmail.com>
2023-01-05 19:47:45 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _puts_recursion(s + 1);
-        _putchar(*s);
-    }
+	if (*s == '\0')
+		_putchar(10);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
  }
