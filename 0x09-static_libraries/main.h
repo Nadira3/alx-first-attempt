5c0c86af6a56e29dc7d114a521f901e6ec92208d
Precious Aitanun <paitanun35@gmail.com>
2023-01-06 18:17:39 -0800

    correction

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,3 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
