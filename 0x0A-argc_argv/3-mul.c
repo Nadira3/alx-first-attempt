04b79c4c1dc8441b447ef60c02984703f80f3649
Precious Aitanun <paitanun35@gmail.com>
2023-01-07 01:49:11 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	long int mul = 1;
+	int mul;
 
-	if (argc == 1)
+	if (argc < 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
-	mul = atoi(argv[1]) * atoi(argv[2];
-	printf("%ld\n", mul);
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
 	return (0);
 }
