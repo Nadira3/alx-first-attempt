021309a006d11a76fb424238faf0eb1f9d2321d0
Precious Aitanun <paitanun35@gmail.com>
2023-01-06 20:57:54 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	long int add = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", add);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			add *= atoi(argv[i]);
+		}
+		printf("%ld\n", add);
+	}
+	return (0);
+}
