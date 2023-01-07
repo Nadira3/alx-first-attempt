e6cee0a9f90d2ea82a4afa40267fcda71ca571f3
Precious Aitanun <paitanun35@gmail.com>
2023-01-06 20:46:52 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc == 1)
+		printf("Error\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
